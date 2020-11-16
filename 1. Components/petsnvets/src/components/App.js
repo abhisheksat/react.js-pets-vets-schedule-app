40707f7818f80a22c96e75089147c24dbaf210e2
abhisheksat <abhisheksatpute11@gmail.com>
2020-11-16 18:35:35 -0500

    Components and JSON data

---
+++ 1. Components/petsnvets/src/components/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import AddAppointments from './AddAppointments'
+import ListAppointments from './ListAppointments'
+import SearchAppointments from './SearchAppointments'
+
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        });
+      });
+  }
+
+  render() {
+
+    const listItems = this.state.myAppointments.map(item => (
+      <div>
+        <div>{item.petName}</div>
+        <div>{item.ownerName}</div>
+      </div>
+    ));
+
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {listItems}
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+  }
+}
+
+export default App;
