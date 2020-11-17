92f6fa30b8a36df70e7bba65c548498f6737b58b
abhisheksat <abhisheksatpute11@gmail.com>
2020-11-16 19:58:23 -0500

    Adding Events, Delete with lodash {without}

---
+++ 4. Adding Events/petsnvets/src/components/App.js
@@ -0,0 +1,64 @@
+import { without } from 'lodash';
+import React, { Component } from 'react';
+import '../css/App.css';
+import AddAppointments from './AddAppointments'
+import ListAppointments from './ListAppointments'
+import SearchAppointments from './SearchAppointments'
+
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: [],
+      lastIndex: 0
+    };
+
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+  }
+
+  deleteAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts
+    });
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({lastIndex: this.state.lastIndex + 1});
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        });
+      });
+  }
+
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments appointments={this.state.myAppointments}
+                  deleteAppointment={this.deleteAppointment}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+  }
+}
+
+export default App;
